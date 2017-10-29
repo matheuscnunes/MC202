1e4ef53123bf8a59edb8ee39f5dae7cd8965cc2b
Mateus Freitas <mateusfreitas.silveira@gmail.com>
2017-10-28 23:48:38 -0200

    Lab07 and c program template

---
+++ lab07/lab07.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Nomes: Matheus Cezar Nunes , Mateus Freitas Silveira
+ * RA: 203373 e 203263
+ *
+ */
+
+//Enuns
+typedef enum Operacoes {
+  
+} Operacoes;
+typedef enum bool { false, true } bool;
+
+//Structs
+struct No {
+ 	struct No *dir, *esq, *pai;
+};
+
+typedef struct No No;
+typedef struct Programa Programa;
+
+//Esqueletos
+
+int main() {
+
+	int op;
+	// Le as operações do sistema
+	while (scanf("%d", &op) != EOF) {
+		switch (op) {
+		  default:
+		    printf("Operação não cadastrada!\n");
+		  break;
+		}
+	}
+
+	free(estados);
+	// Liberar árvore completa
+}
